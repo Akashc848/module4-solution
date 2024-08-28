9f3f9ecec285b943eb27e305b3a571fc7b9167e4
Akashc848 <111580233+Akashc848@users.noreply.github.com>
2024-08-28 10:29:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
-
+  
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
@@ -63,5 +63,6 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker.speak(names[i]);
   }
 }
+})()
 
-})();
+
